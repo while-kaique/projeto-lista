7b3b666ada0a2baad1ff6f8ac58c2633d5c76036
while-kaique <123902283+while-kaique@users.noreply.github.com>
2023-02-04 13:51:32 -0300

    Criei o site!

---
+++ script.js
@@ -0,0 +1,72 @@
+let num = window.document.querySelector('input#num')
+let lista = document.querySelector('select#lista')
+let res = document.querySelector('div#cad')
+let valores = []
+
+function isNumber(num) {
+    if (num.value >= 1 && num.value <= 100 ){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(num) {
+    if (valores.indexOf(Number(num.value)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar(){
+    if (isNumber(num) && !inLista(num)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor adicionado ${num.value}`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }   
+
+    num.value = ''
+    
+    num.focus()
+
+}
+
+function finalizar(){
+    if (valores.length == 0) {
+        window.alert('Adicione algo na lista!')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+
+        for (var t = 0; t < total; t++ ) {
+            soma += valores[t]
+            if (valores[t] > maior ) {
+                maior = valores[t]
+            } 
+
+            if (valores[t] < menor) {
+                menor = valores[t]
+            }
+        }
+        
+        let media = soma / valores.length
+        let medias = media + ''
+        medias.replace(".", ",")
+        
+        res.innerHTML = `O total de números na lista é ${total}.`
+        res.innerHTML += `<br>O menor número na lista é ${menor}.`
+        res.innerHTML += `<br>O maior número na lista é ${maior}.`
+        res.innerHTML += `<br>A soma de número na lista é ${soma}.`
+        res.innerHTML += `<br>A média dos números na lista é ${
+            medias.replace(".", ",")}.`
+        
+    }
+    valores = []
+
+}
